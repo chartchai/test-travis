702e49686f93c02f36301794569f7e94d47f11ec
Chartchai <chartchai.d@cmu.ac.th>
2017-01-30 07:23:06 +0700

    initial test

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
+var minimist = require('minimist');
+var args = minimist(process.argv.slice(2));
+gulp.task('deploy', function() {
+  var remotePath = '/';
+  var conn = ftp.create({
+    host: '54.254.254.167',
+    user: args.user,
+    password: args.password,
+    log: gutil.log
+  });
+  gulp.src(['index.html', './**/*.css'])
+    .pipe(conn.newer(remotePath))
+    .pipe(conn.dest(remotePath));
+});
